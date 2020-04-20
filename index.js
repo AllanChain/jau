85827e22839a9fb31fb1cec6e43ffabfb78fd1e2
Allan Chain <allanchain@pku.edu.cn>
2020-04-20 22:14:41 +0800

    Feature: group and unique

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-module.exports = (arr, keys) => {
-  let comp
+const parseFunc = keys => {
   if (typeof keys === 'function') {
-    comp = keys
-  } else if (Array.isArray(keys)) {
-    comp = (a, b) => keys.every(key => a[key] === b[key])
-  } else if (keys === undefined) {
-    comp = (a, b) => a === b
-  } else {
-    comp = (a, b) => a[keys] === b[keys]
+    return keys;
   }
-  return arr.filter((a, i) => arr.findIndex(b => comp(a, b)) === i)
-}
+
+  if (Array.isArray(keys)) {
+    return (a, b) => keys.every(key => a[key] === b[key]);
+  }
+
+  if (keys === undefined) {
+    return (a, b) => a === b;
+  }
+
+  return (a, b) => a[keys] === b[keys];
+};
+
+const pushDefault = (array, i, element) => {
+  if (i in array) {
+    array[i].push(element);
+  } else {
+    array.push([element]);
+  }
+};
+
+module.exports = {
+  unique(array, keys) {
+    const comp = parseFunc(keys);
+    return array.filter((a, i) => array.findIndex(b => comp(a, b)) === i);
+  },
+  group(array, keys) {
+    const comp = parseFunc(keys);
+    const result = [];
+    array.forEach(a => pushDefault(result, array.findIndex(b => comp(a, b)), a));
+    return result;
+  }
+};
--- test.js
+++ test.js
@@ -1,18 +1,45 @@
-const test = require('ava')
-const jau = require('.')
+const test = require('ava');
+const jau = require('.');
 
-test('no param', t => {
-	t.deepEqual(jau([1, 2, 2, 3, 1, 2, 4]), [1, 2, 3, 4])
-	t.deepEqual(jau(['a', 'a', 'b', 'a', 'c', 'a', 'd']), ['a', 'b', 'c', 'd'])
-})
-test('with property name', t => {
-  t.deepEqual(jau([{a: 1}, {a: 1, b: 2}, {b: 2}], 'a'), [{a: 1}, {b: 2}])
-  t.deepEqual(jau([{a: 1}, {a: 1, b: 2}, {b: 2}], 'b'), [{a: 1}, {a: 1, b: 2}])
-})
-test('with properties', t => {
-  t.deepEqual(jau([{a: 1, b: 2, c: 1}, {a: 1, b: 2}, {b: 2}], ['a', 'b']), [{a: 1, b: 2, c: 1}, {b: 2}])
-})
-test('with function', t => {
-  t.deepEqual(jau([{a: 1, b: 2, c: 1}, {a: 1, b: 2}, {b: 2}], 
-    (a, b) => a.a === b.a || a.b === b.b), [{a: 1, b: 2, c: 1}])
-})
+test('group: no param', t => {
+  t.deepEqual(jau.group([1, 2, 2, 3, 1, 2, 4]),
+    [[1, 1], [2, 2, 2], [3], [4]]);
+  t.deepEqual(jau.group(['a', 'a', 'b', 'a', 'c', 'a', 'd']),
+    [['a', 'a', 'a', 'a'], ['b'], ['c'], ['d']]);
+});
+test('group: with property name', t => {
+  t.deepEqual(jau.group([{a: 1}, {a: 1, b: 2}, {b: 2}], 'a'),
+    [[{a: 1}, {a: 1, b: 2}], [{b: 2}]]);
+  t.deepEqual(jau.group([{a: 1}, {a: 1, b: 2}, {b: 2}], 'b'),
+    [[{a: 1}], [{a: 1, b: 2}, {b: 2}]]);
+});
+test('group: with properties', t => {
+  t.deepEqual(jau.group([{a: 1, b: 2, c: 1}, {a: 1, b: 2}, {b: 2}], ['a', 'b']),
+    [[{a: 1, b: 2, c: 1}, {a: 1, b: 2}], [{b: 2}]]);
+});
+test('group: with function', t => {
+  t.deepEqual(jau.group([{a: 1, b: 2, c: 1}, {a: 1, b: 2}, {b: 2}],
+    (a, b) => a.a === b.a || a.b === b.b),
+  [[{a: 1, b: 2, c: 1}, {a: 1, b: 2}, {b: 2}]]);
+});
+
+test('unique: no param', t => {
+  t.deepEqual(jau.unique([1, 2, 2, 3, 1, 2, 4]),
+    [1, 2, 3, 4]);
+  t.deepEqual(jau.unique(['a', 'a', 'b', 'a', 'c', 'a', 'd']),
+    ['a', 'b', 'c', 'd']);
+});
+test('unique: with property name', t => {
+  t.deepEqual(jau.unique([{a: 1}, {a: 1, b: 2}, {b: 2}], 'a'),
+    [{a: 1}, {b: 2}]);
+  t.deepEqual(jau.unique([{a: 1}, {a: 1, b: 2}, {b: 2}], 'b'),
+    [{a: 1}, {a: 1, b: 2}]);
+});
+test('unique: with properties', t => {
+  t.deepEqual(jau.unique([{a: 1, b: 2, c: 1}, {a: 1, b: 2}, {b: 2}], ['a', 'b']),
+    [{a: 1, b: 2, c: 1}, {b: 2}]);
+});
+test('unique: with function', t => {
+  t.deepEqual(jau.unique([{a: 1, b: 2, c: 1}, {a: 1, b: 2}, {b: 2}],
+    (a, b) => a.a === b.a || a.b === b.b), [{a: 1, b: 2, c: 1}]);
+});
